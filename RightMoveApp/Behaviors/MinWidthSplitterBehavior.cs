3c4232db5c0c8d7183cef9e14c3c0c1c9a1d5d3b
Billy Maat <bsmaat@gmail.com>
2021-12-11 20:24:28 +0000

    Added MinWidthConverter.cs and made changes in MainWindow.xaml Added MinWidthSplitterBevahior

---
+++ RightMoveApp/Behaviors/MinWidthSplitterBehavior.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RightMoveApp.Behaviors
+{
+    public class MinWidthSplitterBehavior : Behavior<Grid>
+    {
+        public Grid ParentGrid { get; set; }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            ParentGrid = this.AssociatedObject as Grid;
+            ParentGrid.SizeChanged += Parent_SizeChanged;            
+        }
+
+        private void Parent_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (ParentGrid.ColumnDefinitions.Count == 3)
+            {
+                Double maxW = e.NewSize.Width - ParentGrid.ColumnDefinitions[2].MinWidth -
+                              ParentGrid.ColumnDefinitions[1].ActualWidth;
+                ParentGrid.ColumnDefinitions[0].MaxWidth = maxW;
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            if (ParentGrid != null)
+            {
+                ParentGrid.SizeChanged -= Parent_SizeChanged;
+            }
+        }
+    }
+}
---
+++ RightMoveApp/UserControls/ValueConverters/MinWidthConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace RightMoveApp.UserControls.ValueConverters
+{
+	public class MinWidthConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value == null ? 0 : 200; 
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
