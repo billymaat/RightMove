29efdf87390c1f17b8071957a8564945f5e311c0
billymaat <bsmaat@gmail.com>
2024-06-14 15:13:08 +0100

    quick fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # build the solution
-cd ../
 dotnet build RightMove.sln --configuration Release
 tar -czvf rightmove.tar.gz RightMoveConsole/bin/Release/net8.0/.
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # build the solution
-cd ../
 tar -czvf rightmove.tar.gz RightMoveConsole/bin/Release/net8.0/.
