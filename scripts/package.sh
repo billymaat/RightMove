c8611934eb9ef854d762b238144bd28a2ce11046
billymaat <bsmaat@gmail.com>
2024-06-15 17:47:32 +0100

    update package

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # package the solution
-tar -czvf rightmove_console.tar.gz RightMoveConsole/bin/Release/net8.0/.
-tar -czvf rightmove_webapi.tar.gz RightMove.Web/bin/Release/net8.0/.
+tar -czvf rightmove_console.tar.gz -C RightMoveConsole/bin/Release/net8.0/ .
+tar -czvf rightmove_webapi.tar.gz -C RightMove.Web/bin/Release/net8.0/ .
