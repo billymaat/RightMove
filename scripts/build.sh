b71da56ddebbe682c074f1feae1bb90bdfc1beb0
billymaat <bsmaat@gmail.com>
2024-06-15 17:38:15 +0100

    package + artifact

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,3 @@
 # build the solution
 echo $BUILD_NUMBER
 dotnet build RightMove.sln --configuration Release /p:AssemblyVersion=1.1.1.$BUILD_NUMBER
-tar -czvf rightmove.tar.gz RightMoveConsole/bin/Release/net8.0/.
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# build the solution
-tar -czvf rightmove.tar.gz -C RightMoveConsole/bin/Release/net8.0/ .
+# package the solution
+tar -czvf rightmove_console.tar.gz RightMoveConsole/bin/Release/net8.0/.
+tar -czvf rightmove_webapi.tar.gz RightMove.Web/bin/Release/net8.0/.
