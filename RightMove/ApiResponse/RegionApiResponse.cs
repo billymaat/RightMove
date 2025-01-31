ee16ea86f77ec9d49093172d2136a9e155ea9d65
Billy Maat <bsmaat+2@gmail.com>
2025-01-31 20:45:37 +0000

    First bit of region scraping

---
+++ RightMove/ApiResponse/RegionApiResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightMove.ApiResponse
+{
+    public class RegionApiResponse
+    {
+        public Match[] matches { get; set; }
+    }
+
+    public class Match
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public string displayName { get; set; }
+        public string highlighting { get; set; }
+        public Highlight[] highlights { get; set; }
+    }
+
+    public class Highlight
+    {
+        public string text { get; set; }
+        public bool highlighted { get; set; }
+    }
+
+}
---
+++ RightMoveTests/RightMoveRegionServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RightMove.Tests
+{
+    public class RightMoveRegionServiceTests
+    {
+
+        [Test]
+        public void TestMethod()
+        {
+            var rightMoveRegioNService = new RightMoveRegionService();
+            var val = rightMoveRegioNService.Search("London").GetAwaiter().GetResult();
+        }
+    }
+}
